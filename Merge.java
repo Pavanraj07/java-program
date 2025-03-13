2f40bf8088dfca35a3f1beceb2ab8829bc6eff58
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-13 15:37:44 +0530

    commit

---
+++ Merge.java
@@ -0,0 +1,14 @@
+public class Merge {
+    public static void main(String[] args) {
+        int x=1234;
+        int y=77;
+        int a=y;
+        while(y>0){
+            x=x*10;
+            y=y/10;
+        }
+        x=x+a;
+        System.out.println(x);
+    }
+    
+}
