9520444b1cd4713f4984dd39679ba6e027dc6ba3
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-14 14:26:25 +0530

    commit

--- Methods/FirstNum.java
+++ Methods/FirstNum.java
@@ -1,5 +1,20 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class FirstNum {
-    
+    static int remove(int x){
+        int num=0;
+        while(x>0){
+            num=x%10;
+            x=x/10;
+        }
+        return num;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int a=sc.nextInt();
+        System.out.println("Result is:"+remove(a));
+    }
 }
