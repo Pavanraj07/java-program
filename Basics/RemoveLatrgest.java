c642c8ae4227617834ba713cb9a49685624e4954
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-14 12:05:47 +0530

    commit

--- Basics/RemoveLatrgest.java
+++ Basics/RemoveLatrgest.java
@@ -1,4 +1,5 @@
 package Basics;
+
 public class RemoveLatrgest {
     public static void main(String[] args) {
         int num=1234;
--- Methods/lastnum.java
+++ Methods/lastnum.java
@@ -1,5 +1,20 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class lastnum {
-    public int remove
+    static int remove(int x){
+        int num=0;
+        num= x%10;
+        return num;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int a=sc.nextInt();
+        int c=remove(a);
+        
+        System.out.println("Result is:"+c );
+    }
 }
