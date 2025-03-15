f53f9128e9c030f8b94ba7aa5c14953530395be5
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-15 21:14:01 +0530

    commit

---
+++ NumberLogic/SumofDigits.java
@@ -0,0 +1,17 @@
+package NumberLogic;
+
+public class SumofDigits {
+    static int add(int x){
+        int rem=0,sum=0;
+        while(x>0){
+            rem=x%10;
+            x=x/10;
+            sum=sum+rem;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int n=12345;
+        System.out.println(add(n));
+    }
+}
