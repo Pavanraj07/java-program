284ddc096e4f727f87bdc550472d7c3b0720e5ad
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-15 09:09:07 +0530

    commit

--- Basics/RemoveLa/RemoveLargest.java
+++
@@ -1,8 +0,0 @@
-package Basics;
-public class RemoveLatrgest {
-    public static void main(String[] args) {
-        int num=1234;
-        int res=num/10;
-        System.out.println(res);
-    }
-}
