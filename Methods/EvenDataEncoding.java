3f9653c3116b533b85dc13bfd852260705baf303
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-15 12:09:23 +0530

    commit

---
+++ Methods/EvenDataEncoding.java
@@ -0,0 +1,24 @@
+package Methods;
+
+public class EvenDataEncoding {
+    static void result(int x){
+        int rem=0,rev=0;
+        while(x>0){
+            rem=x%10;
+            x=x/10;
+            rev=rev*10+rem;
+        }
+        int rem1=0;
+        System.out.println("Encoded value is:");
+        while(rev>0){
+            rem1=rev%10;
+            rev=rev/10;
+            System.out.print((rem1%2==0)? 0 : 1);
+        }
+}
+    public static void main(String[] args) {
+        int n=125369;
+        System.out.println("Number is :"+n);
+        result(n);
+    }
+}
