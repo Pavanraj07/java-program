12bd98506c25df39b4ff1324ca4a9bff52e66895
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-15 16:15:26 +0530

    commit

---
+++ NumberLogic/NaturalNumber.java
@@ -0,0 +1,15 @@
+package NumberLogic;
+
+public class NaturalNumber {
+    static int add(int x){
+        int i,sum=0;
+        for(i=0;i<=x;i++)
+            sum=sum+i;
+        
+        return sum;
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(add(n));
+    }
+}
