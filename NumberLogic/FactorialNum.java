5d03e92a5ac10901ef383a92fb38c4f40e847a5f
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-15 21:23:01 +0530

    commit

---
+++ NumberLogic/FactorialNum.java
@@ -0,0 +1,12 @@
+package NumberLogic;
+
+public class FactorialNum {
+    static int Fact(int x){
+        if(x==1)return 1;
+        else return x*Fact(x-1);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(Fact(n));
+    }
+}
