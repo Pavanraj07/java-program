8ac37eef466551c4a1329e2016780ceac8a7c1a8
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-16 19:32:38 +0530

    commit

---
+++ NumberLogic/LCM.java
@@ -0,0 +1,23 @@
+package NumberLogic;
+
+public class LCM {
+    static int GCD(int x,int y){
+        int res=0;
+        while(x>0){
+            res=x%y;
+            x=y;
+            y=res;
+            if(res==0)
+                break;
+        }
+        return x;
+    }
+    static int CalculateLCM(int x,int y){
+        int res= x*y;
+        return res/GCD(x,y);
+    }
+    public static void main(String[] args) {
+        int x=4 , y=8;
+        System.out.println("LCM is:"+CalculateLCM(x,y));
+    }
+}
