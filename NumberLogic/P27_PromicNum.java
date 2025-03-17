f520e02eec1ba74c6e081a95cf8169ec70347297
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-17 16:01:05 +0530

    commit

---
+++ NumberLogic/P27_PromicNum.java
@@ -0,0 +1,20 @@
+package NumberLogic;
+
+public class P27_PromicNum {
+    static void pronic(int n){
+        int mul=0;
+        for(int i=0;i<=n/2;i++){
+            mul=i*(i+1);
+            if(mul==n)
+                break;
+        }
+        if(mul==n)
+            System.out.println("Pronic number");
+        else
+            System.out.println("Not a pronic number");
+    }
+    public static void main(String[] args) {
+        int n=2;
+        pronic(n);
+    }
+}
