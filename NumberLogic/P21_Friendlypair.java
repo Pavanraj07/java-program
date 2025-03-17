fd5287e5eb126d3573c0274c1f5e382e9ce5717a
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-17 12:39:36 +0530

    commit

--- NumberLogic/P21_Friendlypair.java
+++ NumberLogic/P21_Friendlypair.java
@@ -2,19 +2,24 @@ package NumberLogic;
 
 public class P21_Friendlypair {
     static void pair(int m,int n){
-        int sum1=0;
-        int sum2=0;
+        int sum1=0,div1=0;
+        int sum2=0,div2=0;
         for(int i=1;i<=m;i++){
             if(m%i==0)
                 sum1+=i;
+            div1=sum1/m;
         }
-        System.out.println(sum1);
 
         for(int i=1;i<=n;i++){
             if(n%i==0)
                 sum2+=i;
+            div2=sum2/n;
         }
-        System.out.println(sum2);
+        if(div1==div2)
+            System.out.println("Friendly pair");
+        else    
+            System.out.println("Not Friendly pair");
+        
     }
     public static void main(String[] args) {
         int m=6 , n=28;
