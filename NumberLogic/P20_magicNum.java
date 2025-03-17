316a37dfb52b1e4ff231c629f5afdbb2e5044fc6
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-17 11:58:42 +0530

    commit

---
+++ NumberLogic/P20_magicNum.java
@@ -0,0 +1,18 @@
+package NumberLogic;
+
+public class P20_magicNum {
+    static int MagicNumber(int n){
+        int sum = 0;
+        while(n > 0) {
+            int rem = n % 10;
+            n /= 10;
+            sum += rem;
+        }
+        return (sum < 10) ? sum : MagicNumber(sum);
+    }
+    public static void main(String[] args) {
+        int n=19;
+        int res = (MagicNumber(n));
+        System.out.println((res == 1) ? "It is a Magic Number" : "It is Not a Magic Number");
+    }
+}
