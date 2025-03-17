385188b2683c9e761b955e3670cab4a5981d6df3
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-17 12:30:54 +0530

    commit

--- NumberLogic/P13_PerfectSquare.java
+++ NumberLogic/P13_PerfectSquare.java
@@ -3,7 +3,7 @@ package NumberLogic;
 public class P13_PerfectSquare {
     static void PerfectSquare(int n){
         int i,flag=0;
-        for(i=2;i<=n;i++){
+        for(i=2;i<=n/2;i++){
             if(i*i==n)
                 flag=1;
         }
@@ -13,7 +13,7 @@ public class P13_PerfectSquare {
             System.out.println("It is not a perfect square");
     }
     public static void main(String[] args) {
-        int n=10;
+        int n=49;
         PerfectSquare(n);
     }
 }
