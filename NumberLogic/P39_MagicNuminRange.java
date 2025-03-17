a23421895398df969ee725970b549a9a317b7ca7
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-17 19:59:53 +0530

    commit

---
+++ NumberLogic/P39_MagicNuminRange.java
@@ -0,0 +1,22 @@
+package NumberLogic;
+
+public class P39_MagicNuminRange {
+    static int MagicNumber(int n){
+        int sum = 0;
+        while(n > 0) {
+            int rem = n % 10;
+            n /= 10;
+            sum += rem;
+        }
+        return (sum >9) ?MagicNumber(sum):sum;
+    }
+    public static void main(String[] args) {
+        int n=19;
+        System.out.println("Magic Numbers are:");
+        for(int i=1;i<=n;i++){
+            int res = MagicNumber(n);
+            if(res==1)System.out.print(i+" ");
+        }
+        
+    }
+}
