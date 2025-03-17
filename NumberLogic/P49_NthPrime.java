e3912a85341415e1b8b610c45a08d6607fd2c8ff
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-17 21:53:40 +0530

    commit

---
+++ NumberLogic/P49_NthPrime.java
@@ -0,0 +1,32 @@
+package NumberLogic;
+
+import java.util.Scanner;
+
+public class P49_NthPrime {
+    static int prime(int n){
+        int flag=0;
+        for(int i=2;i<n/2 ; i++){
+            if(n%i==0){
+                flag=1;
+                break;
+            }
+        }
+        return (flag==1)?0:n;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter n: ");
+        int n= sc.nextInt();
+
+        System.out.println("prime number is:");
+        int res=0 , i=0;
+        while(n>0){
+            res=prime(i);
+            if(res!=0)
+                n--;
+                i++;
+        }
+        System.out.println(res);
+    }
+}
