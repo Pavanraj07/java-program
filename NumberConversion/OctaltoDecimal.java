e0b50806a8d81456e0ec2c5e8b4d22f0558cbae2
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-18 14:33:41 +0530

    commit

---
+++ NumberConversion/OctaltoDecimal.java
@@ -0,0 +1,15 @@
+package NumberConversion;
+
+public class OctaltoDecimal {
+    public static void main(String[] args) {
+        int n=512;
+        int res=0,power=1;
+        while(n>0){
+            int digit=n%10;
+            res+=(digit*power);
+            n/=10;
+            power*=8;
+        }
+        System.out.println(res);
+    }
+}
