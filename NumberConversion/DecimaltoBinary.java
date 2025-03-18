4b75d8113a4639271d621dc417ac2f9bd2fb5219
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-18 12:38:43 +0530

    commit

---
+++ NumberConversion/DecimaltoBinary.java
@@ -0,0 +1,15 @@
+package NumberConversion;
+
+public class DecimaltoBinary {
+    public static void main(String[] args) {
+        int n=13;
+        int res=0,power=1;
+        while(n>0){
+            int digit=n%2;
+            res+=(digit*power);
+            n/=2;
+            power*=10;
+        }
+        System.out.println(res);
+    }
+}
