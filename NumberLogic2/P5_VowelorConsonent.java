fdf16ddd38a6e91e6b8a1c3ab2bf6830ad5a5f83
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-19 10:16:37 +0530

    commit

---
+++ NumberLogic2/P5_VowelorConsonent.java
@@ -0,0 +1,16 @@
+package NumberLogic2;
+
+import java.util.Scanner;
+
+public class P5_VowelorConsonent {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a character:");
+        char ch=sc.next().charAt(0);
+
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+            System.out.println(ch+" is Vowel");
+        else        
+            System.out.println(ch+" is Consonent");
+    }
+}
