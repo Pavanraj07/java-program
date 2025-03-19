13420a5aef9af45095d4678826f6b4ec75a75115
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-19 11:31:34 +0530

    commit

---
+++ NumberLogic2/P6_Alphabet.java
@@ -0,0 +1,18 @@
+package NumberLogic2;
+
+import java.util.Scanner;
+
+public class P6_Alphabet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter:");
+        char ch=sc.next().charAt(0);
+
+        int temp=ch;
+        if((temp>=65 && temp<=90) || (temp>=97 && temp<=122))
+            System.out.println("It is Alphabet");
+        else
+            System.out.println("Not alphabet");
+
+    }
+}
