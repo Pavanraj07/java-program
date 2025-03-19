c149e3c35b968f620bd846e8f7f837f4275c0745
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-19 14:35:10 +0530

    commit

---
+++ BitManipulation/CountSetBit.java
@@ -0,0 +1,20 @@
+package BitManipulation;
+
+import java.util.Scanner;
+
+public class CountSetBit {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter number: ");
+    int n = sc.nextInt();
+
+    int temp= n ,count=0;
+    while(n>0){
+        if((temp&1)==1)count++;
+        temp=n>>1;
+        n=n>>1;
+    }
+    System.out.println("Count of Set bit is:"+count);
+    }
+
+}
