60c0c2f65b3d3631df2a94384d53cbc2cb5eefef
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-19 07:56:45 +0530

    commit

---
+++ NumberLogic2/P3_SumofDigitsReecursion.java
@@ -0,0 +1,23 @@
+package NumberLogic2;
+
+import java.util.Scanner;
+
+public class P3_SumofDigitsReecursion {
+    static int SumogDigit(int n){
+        int rem=0,sum=0;
+        if(n>0){
+            rem=n%10;
+            n=n/10;
+            sum=sum+rem;
+            return rem+SumogDigit(n);
+        }
+        else
+            return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter m:");
+        int n=sc.nextInt();
+        System.out.println(SumogDigit(n));
+    }
+}
