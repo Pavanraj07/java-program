90767574fb8de56aef44beee241738561dd87ebc
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-19 14:35:16 +0530

    commit

---
+++ BitManipulation/SetithBit.java
@@ -0,0 +1,16 @@
+package BitManipulation;
+import java.util.Scanner;
+public class SetithBit {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int n = sc.nextInt();
+        System.out.print("Enter i: ");
+        int i = sc.nextInt();
+
+        int temp=0;
+        temp=1<<i;
+        int res=n | temp;
+        System.out.println(res);
+    }
+}
