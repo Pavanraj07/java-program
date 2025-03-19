a836692e4e1fb3f538b43d77d9c78e5b2f07d8b3
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-19 07:48:29 +0530

    commit

--- NumberLogic2/P2_PowerUsingRecursion.java
+++ NumberLogic2/P2_PowerUsingRecursion.java
@@ -1,13 +1,20 @@
 package NumberLogic2;
 
-public class P2_PowerUsingRecursion {
-    static int result(int x,int y){
-        int temp=x;
-        while(y>1){
-            x=temp*x;
-            y--;
-        }
-        return x;
-    }
+import java.util.Scanner;
 
+public class P2_PowerUsingRecursion {
+    static int result(int m,int n){
+        if(n==0) {
+            return 1; 
+        }
+        return m*result(m,n-1); 
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter m:");
+        int m=sc.nextInt();
+        System.out.println("Enter n:");
+        int n=sc.nextInt();
+        System.out.println(result(m,n));
+    }
 }
