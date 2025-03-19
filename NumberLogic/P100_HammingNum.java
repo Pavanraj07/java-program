1cf4da17df5a75006e6aeb8dad1281a0276a1c7b
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-19 09:36:25 +0530

    commit

--- NumberLogic/P100_HammingNum.java
+++ NumberLogic/P100_HammingNum.java
@@ -9,7 +9,7 @@ public class P100_HammingNum {
         while(n%2==0)n/=2;
         while(n%3==0)n/=3;
         while(n%5==0)n/=5;
-        
+
         return (n==1)?n:0;   
     }
     public static void main(String[] args) {
