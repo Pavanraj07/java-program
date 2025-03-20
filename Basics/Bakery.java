df9ea1e71a60962cf2a231534e6deadfc3363c5f
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-20 12:00:04 +0530

    commit

---
+++ Basics/Bakery.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class Bakery {
+    static void chef(){
+        System.out.println("I am chef");
+    }
+    public static void main(String[] args) {
+        System.out.println("Pavan raj");
+        chef();
+    }
+}
