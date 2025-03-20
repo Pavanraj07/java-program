2fe19c71d0cbef5c46a3c231d8ab97c7f3f5af1a
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-20 12:00:32 +0530

    commit

---
+++ NumberLogic2/P117_OctaltoBinary.java
@@ -0,0 +1,26 @@
+package NumberLogic2;
+
+public class P117_OctaltoBinary {
+    static int OctaltoDecimal(int n){
+        int power=1,res=0;
+        while(n>0){
+            int rem=n%10;
+            n/=10;
+            res+=(rem*power);
+            power*=8;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int n=353;
+        int sum=OctaltoDecimal(n);
+        int rem=0,res=0,power=1;
+        while(sum>0){
+            rem=sum%2;
+            sum/=2;
+            res+=(rem*power);
+            power*=10;
+        }
+        System.out.println(res);
+    }
+}
