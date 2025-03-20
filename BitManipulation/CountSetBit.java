26f144c03517c75ee072fd5ca50efb9915186ffb
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-20 08:05:36 +0530

    commit

--- BitManipulation/CountSetBit.java
+++ BitManipulation/CountSetBit.java
@@ -10,9 +10,11 @@ public class CountSetBit {
 
     int temp= n ,count=0;
     while(n>0){
-        if((temp&1)==1)count++;
-        temp=n>>1;
-        n=n>>1;
+        // if((temp&1)==1)count++;
+        // temp=n>>1;
+        // n=n>>1;
+        n=n&(n-1);
+        count++;
     }
     System.out.println("Count of Set bit is:"+count);
     }
