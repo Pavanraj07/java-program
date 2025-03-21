1ead658e7002089199a7bbda24e3ea7f98e9bd0a
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-21 11:26:23 +0530

    commit

---
+++ Pattern/ReverseSequence.java
@@ -0,0 +1,9 @@
+package Pattern;
+
+public class ReverseSequence {
+    public static void main(String[] args) {
+        // int n=4;
+        for(int i=4;i>=0;i--)
+            System.out.print(i+" ");
+    }
+}
