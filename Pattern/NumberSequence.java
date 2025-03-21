bdc0cf6de1bdc19f7f1fb3e10efac66f10e25818
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-21 11:26:17 +0530

    commit

--- Pattern/NumberSequence.java
+++ Pattern/NumberSequence.java
@@ -3,13 +3,15 @@ package Pattern;
 public class NumberSequence {
     static void Sequence(int n){
         for(int i=0;i<n;i++)
-            System.out.print(i+" ");
-        for(int i=n;i>=0;i--)
-            System.out.print(i+" ");
+            if(i<n/2)
+                System.out.print(i+" ");
+            else
+                System.out.print(n-i-1+" ");
+        // for(int i=n;i>=0;i--)
+        //     System.out.print(i+" ");
     }
     public static void main(String[] args) {
-        int n=10;
-        n/=2;
+        int n=11;
         Sequence(n);//0 1 2 3 4 5 4 3 2 1 0 
     }
 }
