af00d7f316bfe0713f70b5a69450d956eeeb6ab9
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-21 11:26:12 +0530

    commit

---
+++ Pattern/MultiplicationTable.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int n=7;
+        for(int i=n;i<=n*10;i+=n){
+            System.out.println(i);
+        }
+        // for(int i=1;i<=10;i++){
+        //     System.out.println(n*i);
+        }
+    }
+}
