1bf622135f210d8da4615957d3e9667f3c9e5ba1
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-21 07:15:56 +0530

    commit

---
+++ NumberLogic2/P127_Multiplywith7.java
@@ -0,0 +1,12 @@
+package NumberLogic2;
+
+import java.util.Scanner;
+
+public class P127_Multiplywith7 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter n:");
+        int n=sc.nextInt();
+        System.out.println("Result is "+n*7);
+    }
+}
