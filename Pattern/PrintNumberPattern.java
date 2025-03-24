3f845dd3cc48389057bb49be468754d3efd053ff
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-24 09:54:00 +0530

    commit

--- Pattern/PrintNumberPattern.java
+++ Pattern/PrintNumberPattern.java
@@ -48,7 +48,7 @@ public class PrintNumberPattern {
                             System.out.print(" ");
                         break;
                     case '6':
-                        if(i==0 || j==0 || i==n-1 || i==(3*n)/4 || j==n-1 &&(i>(n/2)+1))
+                        if(i==0 || j==0 || i==n-1 || i==n/2 || j==n-1 &&(i>(n/2)))
                             System.out.print("*");
                         else   
                             System.out.print(" ");
@@ -80,25 +80,6 @@ public class PrintNumberPattern {
             System.out.println();
         }
     }
+}
 
 
-
-        // for(int i=0;i<n;i++){
-        //     for(int j=-0;j<n;j++){
-        //         // 0:if(i==0 || j==0 || j==n-1 || i==n-1)
-        //         // 1:if(j==n/2 || i==n-1 || i+j==n/2)
-        //         // 2:if(i==0 || i==n-1 || i==n/2 || j==0 &&(i>n/2) || j==n-1 &&(i<n/2))
-        //         // 3:if(i==0 || i==n-1 || j==n-1 || i==n/2)
-        //         // 4:if(j==n/2 || i==n/2 || i+j==n/2)
-        //         // 5:if(i==0 || i==n-1 || i==n/2 || j==0 &&(i<n/2) || j==n-1 &&(i>n/2))
-        //         // 6:if(i==0 || j==0 || i==n-1 || i==(3*n)/4 || j==n-1 &&(i>(n/2)+1))
-        //         // 7:if(i==0 || j==n-1)
-        //         // 8:if(i==0 || j==0 || j==n-1 || i==n-1 || i==n/2)
-        //         if(i==0 || j==n-1 || i==n/2 || j==0 && (i<n/2) )
-        //             System.out.print("*");
-        //         else   
-        //             System.out.print(" ");
-        //         }
-        //         System.out.println();
-        //     }
-    }
