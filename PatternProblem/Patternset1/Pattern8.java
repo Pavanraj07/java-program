9f1f31d6237c57b30d3f3a8ee6c164f5b78e9321
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-25 09:35:33 +0530

    commit

--- PatternProblem/Patternset1/Pattern8.java
+++ PatternProblem/Patternset1/Pattern8.java
@@ -2,15 +2,16 @@ package PatternProblem.Patternset1;
 
 public class Pattern8 {
     public static void main(String[] args) {
-        int n=5 ;
+        int n=5 ,count=0;
         for(int i=0;i<=n;i++){
             for(int j=0;j<=n;j++){
-                if(i-j>0)
-                    System.out.print(i+j+" ");
-                else   
-                    System.out.print(" ");
+                if(i-j>0 ){
+                    count++;
+                    System.out.print(count+" ");  
+                }
+                System.out.print(" ");
             }
             System.out.println();
-        }
+        } 
     }
 }
