f87f3dee3f520b244c29be7161acb959c7421bf7
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-25 12:01:18 +0530

    commit

---
+++ Arrays/Initialization.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Initialization {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array:");
+        int n=sc.nextInt();
+        
+        System.out.println("Enter Array elements:");
+        int[] a = new int[n];
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+        
+        System.out.println("Array elements are:");
+        for(int i=0;i<n;i++)
+            System.out.print(a[i]+" ");
+    }
+}
