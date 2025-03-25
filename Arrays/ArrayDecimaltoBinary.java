253bd19e4fdaf22b9650a9f9670b5da5c7b4e8cc
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-25 15:55:36 +0530

    commit

---
+++ Arrays/ArrayDecimaltoBinary.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ArrayDecimaltoBinary {
+    static int DecimaltoBinary(int n){
+        int res=0,power=1;
+        while(n>0){
+            int digit=n%2;
+            res+=(digit*power);
+            n/=2;
+            power*=10;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array:");
+        int n=sc.nextInt();
+        
+        System.out.println("Enter Array elements:");
+        int[] a = new int[n];
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+        int res=0,i=0;
+        System.out.println("Binary of Array elements are");
+        for(i=0;i<n;i++){
+            res=DecimaltoBinary(a[i]);
+            a[i]=res;
+        }
+        for(i=0;i<n;i++)
+            System.out.print(a[i]+" ");
+    }
+}
