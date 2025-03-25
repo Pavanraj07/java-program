f9fc94da4876a8456b54e1fd1917d2226606da33
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-25 15:56:28 +0530

    commit

---
+++ Arrays/SumofArrayElement.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class SumofArrayElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array:");
+        int n=sc.nextInt();
+        int sum=0;
+        
+        System.out.println("Enter Array elements:");
+        int[] a = new int[n];
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+        
+        System.out.println("Sum is:");
+        for(int i=0;i<n;i++)
+            sum=sum+a[i];
+
+        System.out.print(sum+" ");
+    }
+}
