55b1f3a3d82e5a63bc654647b8b0f345bb3e1a1e
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-25 15:56:33 +0530

    commit

---
+++ Arrays/SearchArrayElement.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class SearchArrayElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array:");
+        int n=sc.nextInt();
+        
+        System.out.println("Enter Array elements:");
+        int[] a = new int[n];
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+        
+        System.out.println("Search index to be searched:");
+        int k=sc.nextInt();
+        int index=0;
+        for(int i=0;i<n;i++)
+            if(a[i]==k)index=i;
+
+        System.out.print(index+" ");
+    }
+}
