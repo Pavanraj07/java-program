d7e2994c1e591bd0cef9e441b45d509de3624239
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-26 09:24:12 +0530

    commit

--- PatternProblem/Patternset1/Pattern12.java
+++ PatternProblem/Patternset1/Pattern12.java
@@ -3,7 +3,7 @@ package PatternProblem.Patternset1;
 public class Pattern12 {
     public static void main(String[] args) {
         int n=9 ;
-        for(int i=0;i<n;i+=2){
+        for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i+j>=n-1)
                     System.out.print((char)('A'+i)+" ");
