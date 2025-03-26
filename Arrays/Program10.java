e16df3f526844515bacc235058240bb703fe8fc8
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-26 09:23:54 +0530

    commit

---
+++ Arrays/Program10.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Program10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array:");
+        int n=sc.nextInt();
+        
+        System.out.println("Enter Array elements:");
+        int[] a = new int[20];
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+        
+        int i=0;
+        for(i=0;i<n;i++){
+            a[i]=a.length%a[i];
+            System.out.println(a[i]+" ");
+        }
+    }
+}
