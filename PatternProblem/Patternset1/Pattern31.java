eb288d149a21f88eed86b79e8bbc64dd4c255685
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-26 09:25:31 +0530

    commit

---
+++ PatternProblem/Patternset1/Pattern31.java
@@ -0,0 +1,17 @@
+package PatternProblem.Patternset1;
+
+public class Pattern31 {
+    public static void main(String[] args) {
+        int n=5 ;
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<=n;j++){
+                if(i+j>n-1)
+                    System.out.print((char)('A'+count++) +" ");
+                else   
+                    System.out.print( " ");
+            }
+            System.out.println();
+        }
+    }
+}
