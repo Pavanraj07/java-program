32517d93e474f771e1e94adbe0daf639e6a09b95
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-26 09:26:43 +0530

    commit

---
+++ PatternProblem/Patternset1/Pattern46.java
@@ -0,0 +1,16 @@
+package PatternProblem.Patternset1;
+
+public class Pattern46 {
+    public static void main(String[] args) {
+        int n=10 ;
+        for(int i=0;i<=n;i++){
+            for(int j=1;j<n;j++){
+                if(i+j==n/2  || i-j==-n/2)
+                    System.out.print(n/2-i+" ");
+                else   
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }      
+}
