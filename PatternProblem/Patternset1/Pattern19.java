a642cf3bbea0fcc4e56f4e11beaf5d764e1e6f70
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-26 09:24:28 +0530

    commit

---
+++ PatternProblem/Patternset1/Pattern19.java
@@ -0,0 +1,18 @@
+package PatternProblem.Patternset1;
+
+public class Pattern19 {
+    public static void main(String[] args) {
+        int n=7 ,res=0;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                if(i-j>=0 && j<=n/2 && i+j<=n-1 && j<=n/2){
+                    res=(n/2)-j-i;
+                    System.out.print(res<0?-res +" ": res+" ");
+                }
+                else   
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
