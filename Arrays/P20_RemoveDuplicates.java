8c461f8f5cb5eeeb3dbcf3e5841a8b6e4e9c000c
Pavanraj <pavanrajcsd152@gmail.com>
2025-03-27 19:47:13 +0530

    commit

---
+++ Arrays/P20_RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class P20_RemoveDuplicates {
+    static int remove(int[] a){
+        int j=1; 
+        int n=a.length;
+        for (int i=1;i<n;i++) {
+            if(a[i]!=a[i-1]) {
+                a[j++]=a[i];
+            }
+        }
+        return j;
+    }
+    public static void main(String[] args) {
+        int[] a={1,2,2,2,6,6,4,5,5};
+        int res=remove(a);
+        for (int i=0;i<res;i++) {
+            System.out.print(a[i]+" ");
+        }
+    }
+}
