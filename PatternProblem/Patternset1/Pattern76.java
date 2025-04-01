63f145587e6c82fbc498d06d037dd5993e482406
Pavanraj <pavanrajcsd152@gmail.com>
2025-04-01 13:45:34 +0530

    commit

--- PatternProblem/Patternset1/Pattern76.java
+++ PatternProblem/Patternset1/Pattern76.java
@@ -6,7 +6,7 @@ public class Pattern76 {
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if( i==n/2 || j==n/2)
-                    System.out.print("* ");
+                    System.out.print("X ");
                 else   
                     System.out.print("  ");
             }
