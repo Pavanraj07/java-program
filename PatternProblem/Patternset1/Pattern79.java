7eac9d86f434bc6e6478a5e4d8a9b23ab4b03d4e
Pavanraj <pavanrajcsd152@gmail.com>
2025-04-02 14:29:14 +0530

    commit

--- PatternProblem/Patternset1/Pattern79.java
+++ PatternProblem/Patternset1/Pattern79.java
@@ -2,11 +2,11 @@ package PatternProblem.Patternset1;
 
 public class Pattern79 {
     public static void main(String[] args) {
-        int n=7 ;
+        int n=11;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(i-j>=0 && j<=n/4)
-                    System.out.print("* ");
+                if((i <= n/4 && j<=n/4) || (i <= n/2 && i>n/4 && j<=n/2) || (i <= 3*n/4 && i>n/4 && i>n/2 && j<=3*n/4))
+                    System.out.print("X ");
                 else   
                     System.out.print("  ");
             }
