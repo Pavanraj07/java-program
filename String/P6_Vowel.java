97a6afed682423169863d7c965d3257f02918a6a
Pavanraj <pavanrajcsd152@gmail.com>
2025-04-03 16:22:36 +0530

    commit

---
+++ String/P6_Vowel.java
@@ -0,0 +1,13 @@
+package String;
+
+public class P6_Vowel {
+    static String vowel(char [] a){
+        for(int i=0;i<a.length;i++){
+            if(v[i]==a)
+                return true;
+        }
+    }
+    public static void main(String[] args) {
+        char a[]={'a','e','i','o','u'};
+    }
+}
