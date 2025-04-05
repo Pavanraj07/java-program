5a2bc596612d1ca889b1a1256fdc465c0ddbdfb4
Pavanraj <pavanrajcsd152@gmail.com>
2025-04-05 09:27:46 +0530

    commit

---
+++ String/P9_RemoveDuplicate.java
@@ -0,0 +1,29 @@
+package String;
+
+public class P9_RemoveDuplicate {
+    static String removedup(char a[]){
+        int n=a.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(a[i]==a[j] )
+                    a[j]='&';
+            }
+        }
+        // for(int i=0;i<n;i++)
+        //     System.out.print(a[i]);
+        // System.out.println();
+       
+        String s="";
+        for(int i=0;i<n;i++){
+            if(a[i]!='&')
+                s+=a[i];
+        }
+        return s;
+    }
+    public static void main(String[] args) {
+        String s="what is yooooour name";
+        char a[]=s.toCharArray();
+        System.out.println(s);
+        System.out.println(removedup(a));
+    }
+}
