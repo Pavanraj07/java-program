aa8779752dfb485e41bc9c7a83cda876cb4dff80
Pavanraj <pavanrajcsd152@gmail.com>
2025-04-07 09:45:46 +0530

    commit

--- BitManipulation/CountSetBit.java
+++ BitManipulation/CountSetBit.java
@@ -8,7 +8,7 @@ public class CountSetBit {
     System.out.println("Enter number: ");
     int n = sc.nextInt();
 
-    int temp= n ,count=0;
+    int count=0;
     while(n>0){
         // if((temp&1)==1)count++;
         // temp=n>>1;
