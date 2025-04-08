49412d96ee9bdf71783041a823fc33832054a4ed
Pavanraj <pavanrajcsd152@gmail.com>
2025-04-08 09:42:26 +0530

    commit

---
+++ PatternAlphabet/PatternX.java
@@ -0,0 +1,17 @@
+package PatternAlphabet;
+
+public class PatternX {
+    public static void main(String[] args) {
+        int n = 15;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i - j == 0 || i + j == n - 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
